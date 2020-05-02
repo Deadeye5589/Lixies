c283dfc63c5573b7f1564fc4c7aecc3b21b14d9f
Deadeye5589 <Deadeye5589@hotmail.com>
2020-05-02 14:42:31 +0200

    First somehow running version
    Get time via NTP
    Store time into DS3231 RTC
    Show time, while blink : each second

---
+++ Software/Lixie Small/Small_Lixie.ino
@@ -0,0 +1,193 @@
+/*
+   ____ ___  ____ _  _     ____ ____ ____ _ ____ _        _  _ ____ _  _ _ ___ ____ ____   /
+   |  | |__] |___ |\ |     [__  |___ |__/ | |__| |        |\/| |  | |\ | |  |  |  | |__/  / 
+   |__| |    |___ | \|     ___] |___ |  \ | |  | |___     |  | |__| | \| |  |  |__| |  \ .  
+
+  (115200 Baud)
+*/                                                                                                                
+
+#include "Lixiesmall.h" // Include Lixie Library
+#include "time_ntp.h"
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <TimeLib.h>
+#include <Timezone.h>
+#include <Wire.h> 
+#include <RtcDS3231.h>
+
+
+// Wifi credentials
+char ssid[] = "Change to your SSID";  
+char pass[] = "Change to your WLAN password";  
+
+// Configure Lixie
+#define DATA_PIN   0 // Pin to drive Lixies
+#define NUM_LIXIES 5  // How many Lixies you have
+Lixie lix(DATA_PIN, NUM_LIXIES);
+
+// Define RTC interrupt pin
+#define RTCsecondInterrupt 13
+
+// Create RTC object for HW I2C
+RtcDS3231<TwoWire> rtcObject(Wire);
+
+// Variables
+volatile bool marker = 1;
+volatile int sekunde = 0;
+volatile bool updateclock = 0;
+uint16_t zeit = 0;
+
+
+// ntp flag
+bool bNTPStarted=false;
+
+//////////////////////////////////////////
+// for time conversion
+//Central European Time (Frankfurt, Paris)
+//////////////////////////////////////////
+TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};     //Central European Summer Time
+TimeChangeRule CET = {"CET ", Last, Sun, Oct, 3, 60};       //Central European Standard Time
+Timezone CE(CEST, CET);
+
+
+///////////////////////////////////////////
+// get UTC time referenced to 1970 by NTP
+///////////////////////////////////////////
+time_t getNTP_UTCTime1970() 
+{ 
+  bNTPStarted=false;  // invalidate; time-lib functions crash, if not initalized poperly
+  unsigned long t = getNTPTimestamp();
+  if (t==0) return(0);
+
+  // scale to 1970 
+  // may look like back & forth with ntp code; wrote it to make needed conversions more clear
+  return(t+946684800UL);
+}
+
+
+// Greet the user.
+void title(){
+  Serial.println(F("----------------------------------"));
+  Serial.println(F(" Lixie Clock                      "));
+  Serial.println(F(" by Christopher                   "));
+  Serial.println(F(" For RTCUSTOMZ of B.I.E.R.        "));
+  Serial.println();
+  Serial.println(F(" V0.1                             "));
+  Serial.println(F("----------------------------------"));
+}
+
+
+// Writes zeroes to all llxie displays.
+void fill_zero(){
+  lix.write(pow(10,(NUM_LIXIES))); // gets number big enough for NUM_LIXIES
+}
+
+void ISR_ATTR SecondsTick(){
+  Serial.println(sekunde);
+  sekunde++;
+  if(sekunde > 60){
+    sekunde = 0;
+    updateclock = 1;
+  }
+  if(marker == 1){
+    marker = 0;
+  }
+  else{
+    marker = 1; 
+  }
+}
+
+
+
+// Setup Routine
+void setup() {
+  Serial.begin(115200);
+
+  title();
+
+  WiFi.begin(ssid, pass);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
+  rtcObject.Begin(); // conneect to RTC
+
+  lix.begin(); // Initialize LEDs
+  lix.brightness(255); // 0-255
+  lix.white_balance(Tungsten100W); // Default
+    // Can be: Tungsten40W, Tungsten100W,  
+    //         Halogen,     CarbonArc,
+    //         HighNoonSun, DirectSunlight,
+    //         OvercastSky, ClearBlueSky  
+    // 2,600K - 20,000K
+  fill_zero();
+
+  if (timeStatus()==timeNotSet)
+    {
+      bNTPStarted=false;
+      Serial.println("Setup sync with NTP service.");
+      setSyncProvider(getNTP_UTCTime1970);
+      setSyncInterval(86400); // NTP re-sync; i.e. 86400 sec would be once per day
+    }
+  else
+    {
+      bNTPStarted=true;   // only position, where we set this flag to true
+    }
+  yield();
+
+  time_t tT = now();
+  time_t tTlocal = CE.toLocal(tT);
+
+  int tag = day(tTlocal);
+  int monat = month(tTlocal);
+  int jahr = year(tTlocal);
+  sekunde = second(tTlocal);
+  int minu = minute(tTlocal);
+  int stunde = hour(tTlocal);
+  Serial.println(tag);
+  Serial.println(monat);
+  Serial.println(jahr);
+  Serial.println(stunde);
+  Serial.println(minu);
+  Serial.println(sekunde);
+
+  yield();
+
+  RtcDateTime currentTime = RtcDateTime(jahr,monat,tag,stunde,minu,sekunde); //define date and time object
+  rtcObject.SetDateTime(currentTime);
+  rtcObject.Enable32kHzPin(false);
+  rtcObject.SetSquareWavePin(DS3231SquareWavePin_ModeClock);
+  rtcObject.SetSquareWavePinClockFrequency(DS3231SquareWaveClock_1Hz);
+  rtcObject.SetIsRunning(true);
+
+  zeit = stunde * 100 + minu;
+
+  yield();
+  
+  // Intro
+  //fill_zero();
+  lix.nixie_mode(true); // Activate Nixie Mode
+  lix.nixie_aura_intensity(8); // Default aural intensity is 8 (0-255)
+
+  //lix.color(CHSV(0,255,255));
+  //lix.color_off(CHSV(127,255,127));
+
+  lix.write(zeit);
+
+  // setup external interupt 
+  attachInterrupt(digitalPinToInterrupt(RTCsecondInterrupt), SecondsTick, FALLING);
+}
+
+void loop() {
+    if(marker == 1)
+      lix.write_fade(zeit+30000);
+    else
+      lix.write_fade(zeit+60000);
+    if(updateclock){
+      updateclock = 0;
+      RtcDateTime currentTime = rtcObject.GetDateTime();
+      zeit = currentTime.Hour() * 100 + currentTime.Minute();
+      Serial.println(zeit);
+    }
+}
