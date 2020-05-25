494bf37cf5f515e2e5a13db9d97d1d348d0385ac
Deadeye5589 <Deadeye5589@hotmail.com>
2020-05-25 13:23:04 +0200

    Lib working correct
    Lib working correctly now
    Added idle animatin while Wifi connects

--- Software/Lixie Big/Lixiesbig.h
+++ Software/Lixie Big/Lixiesbig.h
@@ -1,125 +1,104 @@
 /*
-Lixie.h - Library for driving Lixie displays!
-Created by Connor Nishijima, October 26th 2016.
-Released under the GPLv3 license.
+  Lixie_II.h - Library for controlling the Lixie II!
+  
+  Created by Connor Nishijma July 6th, 2019
+  Released under the GPLv3 License
 */
 
-#ifndef Lixiesbig_h
-#define Lixiesbig_h
+#ifndef lixiesbig_h
+#define lixiesbig_h
 
 #include "Arduino.h"
 
-#ifdef ESP8266
-	#define FASTLED_ESP8266_RAW_PIN_ORDER
-	#define FASTLED_ALLOW_INTERRUPTS 0
-	#define FASTLED_INTERRUPT_RETRY_COUNT 0
+#if defined(ARDUINO_ARCH_ESP8266) || defined(ARDUINO_ARCH_ESP32)
+  #include <Ticker.h> // ESP ONLY
+  // FastLED has issues with the ESP8266, especially
+  // when used with networking, so we fix that here.
+  #define FASTLED_ESP8266_RAW_PIN_ORDER
+  #define FASTLED_ALLOW_INTERRUPTS    0
+  #define FASTLED_INTERRUPT_RETRY_COUNT 0
 #endif
 
+// Aside from those issues, it's my tool of choice for WS2812B
 #include "FastLED.h"
 
-#ifndef LED_TYPE
-  #define LED_TYPE WS2812B
-#endif
+#define ON  1
+#define OFF 0
 
-#ifndef COLOR_ORDER
-  #define COLOR_ORDER GRB
-#endif
+#define INSTANT       0
+#define CROSSFADE     1
 
-class Lixie{
-	public:
-		Lixie(const uint8_t pin, uint8_t nDigits);
-		void begin();
-   
-		void clear(bool show_change = true);
-		void clear_digit(byte index, bool show_change = true);
-		void show();
-		
-		void write(uint32_t input);
-		void write(char* input);
-		
-		void write_flip(char* input, uint16_t flip_time = 100, uint8_t flip_speed = 10);
-		void write_flip(uint32_t input, uint16_t flip_time = 100, uint8_t flip_speed = 10);
-		
-		void write_fade(char* input, uint16_t fade_time = 250);
-		void write_fade(uint32_t input, uint16_t fade_time = 250);
-		
-		void sweep(CRGB col, byte speed = 15);
-		void progress(float percent, CRGB col1, CRGB col2);
-		void fill_fade_in(CRGB col, byte fade_speed = 20);
-		void fill_fade_out(CRGB col, byte fade_speed = 20);
+// Functions
+class Lixie_II
+{
+  public:
+    Lixie_II(const uint8_t pin, uint8_t n_digits);
+    void build_controller(const uint8_t pin);
+    void begin();
+    void transition_type(uint8_t type);
+    void transition_time(uint16_t ms);
+    void max_power(uint8_t V, uint16_t mA);
+    void color_all(uint8_t layer, CRGB col);
+    void color_all_dual(uint8_t layer, CRGB col_left, CRGB col_right);
+    void color_display(uint8_t display, uint8_t layer, CRGB col);
+    void gradient_rgb(uint8_t layer, CRGB col_left, CRGB col_right);
+    void start_animation();
+    void stop_animation();
+    void write(uint32_t input);
+    void write(String input);
+    void write_float(float input, uint8_t dec_places = 1);
+    void clear_all();
+    void write_digit(uint8_t digit, uint8_t num);
+    void push_digit(uint8_t number);
+    void clear_digit(uint8_t digit, uint8_t num);
+    void special_pane(uint8_t index, bool enabled, CRGB col1 = CRGB(0,0,0), CRGB col2 = CRGB(0,0,0));
+    void mask_update();
+    void fade_in();
+    void fade_out();
+    void brightness(float level);
+          void brightness(double level);
+    void run();
+    void wait();
+    void streak(CRGB col, float pos, uint8_t blur);
+    void sweep_color(CRGB col, uint16_t speed, uint8_t blur, bool reverse = false);
+    void sweep_gradient(CRGB col_left, CRGB col_right, uint16_t speed, uint8_t blur, bool reverse = false);
+    void nixie();
+    void white_balance(CRGB c_adj);
+    void rainbow(uint8_t r_hue, uint8_t r_sep);
     
-		void write_digit(byte input, byte index, bool show_change = true);
-		void push_digit(byte number);
+    // ----------------------------------------------
+    // Deprecated Lixie 1 functions and overloads:
+    // ----------------------------------------------
     
-		void color(byte r, byte g, byte b);
-		void color(CRGB c);
-		void color(byte r, byte g, byte b, byte index);
-		void color(CRGB c, byte index);
-
-		void color_off(byte r, byte g, byte b);
-		void color_off(CRGB c);
-		void color_off(byte r, byte g, byte b, byte index);
-		void color_off(CRGB c, byte index);
-		
-		void color_fade(CRGB col, uint16_t duration);
-		void color_fade(CRGB col, uint16_t duration, byte index);
-		
-		void color_array_fade(CRGB *cols, uint16_t duration);
-		void color_array_fade(CHSV *cols, uint16_t duration);
-		
-		void color_wipe(CRGB col1, CRGB col2);
-
-		void nixie_mode(bool enabled, bool has_aura = true);
-		void nixie_aura_intensity(byte val);
-		
-		void brightness(byte bright);
-		void white_balance(CRGB c_adj);
-		
-		void rainbow(uint8_t r_hue, uint8_t r_sep);
-
-		void max_power(byte volts, uint16_t milliamps);
-	
-		uint8_t get_numdigits() const;
-		bool maxed_out(uint32_t input) const;
-
-		CRGB* get_leds() const;
-
-		// Debug Functions
-		void print_binary() const;
-		void print_current() const;
-		uint32_t get_number();
-
-	private:
-		// Order input 0 to 9 = {5, 6, 7, 8, 9, 0, 1, 2, 3, 4}
-		// Order scheiben =     {6, 5, 4, 7, 8, 3, 9, 1, 0, 2 } 
-		static constexpr byte Addresses[10] = {3, 2, 4, 0, 7, 6, 5, 8, 9, 1};
-		const static uint8_t LEDsPerDigit = 30;
-		const uint8_t NumDigits;
-		const uint16_t NumLEDs;
-		CRGB *leds;
-		CRGB *colors;
-		CRGB *colors_off;
-		byte *led_states;
-		byte *digit_brightness;
-		CLEDController *controller;
-		byte sweep_dir = 1;
-		void setBit(uint16_t pos, byte val);
-		byte getBit(uint16_t pos) const;
-		byte get_size(uint32_t input) const;
-		byte char_to_number(char input) const;
-		bool char_is_number(char input) const;
-		void build_controller(uint8_t DataPin);
-		uint32_t current_number = 0;
-		byte current_number_size = 0;
-		byte current_number_arr[20];
-		void store_current(uint32_t input);
-		
-		bool nixie = false;
-		bool nixie_aura = false;
-		byte nixie_aura_level = 0;
-		byte nixie_aura_level_default = 8;
-		CRGB nixie_col_ion = CRGB(255,70,25);
-		CRGB nixie_col_aura = CRGB(45,85,255);
+    void brightness(uint8_t b);
+    void clear(bool show_change = true);
+    void clear_digit(uint8_t index, bool show_change = true);
+    void show();
+    void write_flip(uint32_t input, uint16_t flip_time = 100, uint8_t flip_speed = 10);
+    void write_fade(uint32_t input, uint16_t fade_time = 250);
+    void sweep(CRGB col, uint8_t speed = 15);
+    void progress(float percent, CRGB col1, CRGB col2);
+    void fill_fade_in(CRGB col, uint8_t fade_speed = 20);
+    void fill_fade_out(CRGB col, uint8_t fade_speed = 20);
+    void color(uint8_t r, uint8_t g, uint8_t b);
+    void color(CRGB c);
+    void color(uint8_t r, uint8_t g, uint8_t b, uint8_t index);
+    void color(CRGB c, uint8_t index);
+    void color_off(uint8_t r, uint8_t g, uint8_t b);
+    void color_off(CRGB c);
+    void color_off(uint8_t r, uint8_t g, uint8_t b, uint8_t index);
+    void color_off(CRGB c, uint8_t index);
+    void color_fade(CRGB col, uint16_t duration);
+    void color_fade(CRGB col, uint16_t duration, uint8_t index);
+    void color_array_fade(CRGB *cols, uint16_t duration);
+    void color_array_fade(CHSV *cols, uint16_t duration);
+    void color_wipe(CRGB col1, CRGB col2);
+    void nixie_mode(bool enabled, bool has_aura = true);
+    void nixie_aura_intensity(uint8_t val);
+    
+  private:
+    uint8_t get_size(uint32_t input);
 };
 
 #endif
+	
