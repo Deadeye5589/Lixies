5917338b29696ae41a1d6554a38ba41328f580e9
Pia Piekarek <piapiekarek@googlemail.com>
2020-10-24 17:42:01 +0200

    add basic webserver functionality

--- Software/Lixie Small/Small_Lixie.ino
+++ Software/Lixie Small/Small_Lixie.ino
@@ -1,3 +1,4 @@
+
 /*
    ____ ___  ____ _  _     ____ ____ ____ _ ____ _        _  _ ____ _  _ _ ___ ____ ____   /
    |  | |__] |___ |\ |     [__  |___ |__/ | |__| |        |\/| |  | |\ | |  |  |  | |__/  / 
@@ -9,19 +10,37 @@
 #include "Lixiesmall.h" // Include Lixie Library
 #include "time_ntp.h"
 #include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
-#include <TimeLib.h>
-#include <Timezone.h>
-#include <Wire.h> 
+#include <WiFiUdp.h>    // Built-in Arduino
+#include <TimeLib.h>    // Time   -> Michael Margolis
+#include <Timezone.h>   // Timezone  -> Jack Christensen
+#include <Wire.h>       // Built-in Arduino
 #include <RtcDS3231.h>
+// #include <DS3231.h>  // DS3231 -> Andrew Wickert
 
 
 // Wifi credentials
 char ssid[] = "your_ssid";  
 char pass[] = "your_password";  
 
+// Wifi credentials for access point that is opened if the above ap cannot be connected
+char own_ap_ssid[] = "Lixie";  
+char own_ap_pass[] = "happylixie";  
+
+// Wifi configuration
+IPAddress local_IP(192,168,4,22);
+IPAddress gateway(192,168,4,9);
+IPAddress subnet(255,255,255,0);
+
+
+// Set port of web serve to 80
+WiFiServer server(80);
+
+// Variable for the HTTP Request
+String header;
+
+
 // Configure Lixie
-#define DATA_PIN   0 // Pin to drive Lixies
+#define DATA_PIN   2 // Pin to drive Lixies
 #define NUM_LIXIES 5  // How many Lixies you have
 Lixie_II lix(DATA_PIN, NUM_LIXIES);
 
@@ -77,7 +96,7 @@ time_t getNTP_UTCTime1970()
 void title(){
   Serial.println(F("----------------------------------"));
   Serial.println(F(" Lixie Clock                      "));
-  Serial.println(F(" by Christopher                   "));
+  Serial.println(F(" by Christopher and Pia           "));
   Serial.println(F(" For RTCUSTOMZ of B.I.E.R.        "));
   Serial.println();
   Serial.println(F(" V0.2                             "));
@@ -123,15 +142,16 @@ void setup() {
   // Try to connect to the WLAN
   WiFi.begin(ssid, pass);
   
-   for (int cnt = 0; cnt < 10; cnt++)                                          // check 15 seconds if connected
+  for (int cnt = 0; cnt < 10; cnt++)                                          // check 15 seconds if connected
   {
       lix.sweep_color(CRGB(0,0,255), 20, 5);                                   //Sweep idle
       if (WiFi.status() == WL_CONNECTED)
       {
           wifi_connected = true;
           break;
-      }
-      Serial.println("Connecting...");
+      } 
+      Serial.print("Trying to connect... Try number:");
+      Serial.println(cnt);
       delay(500);
   }
 
@@ -164,13 +184,26 @@ void setup() {
     RtcDateTime currentTime = RtcDateTime(jahr,monat,tag,stunde,minu,sekunde); //define date and time object
     rtcObject.SetDateTime(currentTime);
     zeit = (stunde * 100) + minu;
-  }
+  } 
   // Otherwise we load the time stored in the RTC and hope that it was keeping track 
   else{
     Serial.println("No Wifi, fetching last saved time from RTC");
     RtcDateTime currentTime = rtcObject.GetDateTime();
     zeit = currentTime.Hour() * 100 + currentTime.Minute();
     sekunde = currentTime.Second();
+    
+    Serial.print("Setting soft-AP configuration ... ");
+    Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
+
+    Serial.print("Setting soft-AP ... ");
+
+    bool open_softAP_worked = WiFi.softAP(own_ap_ssid, own_ap_pass); 
+    Serial.println( open_softAP_worked ? "Ready" : "Failed!");
+
+    Serial.print("Soft-AP IP address = ");
+    Serial.println(WiFi.softAPIP());
+    
+    server.begin();
   }
 
   // Set the RTC to give as an interrupt each second
@@ -203,7 +236,59 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(RTCsecondInterrupt), SecondsTick, FALLING);
 }
 
+void makeWebServer(){
+  WiFiClient client = server.available();   // Auf Clients (Server-Aufrufe) warten
+
+  if (client) {                             // Bei einem Aufruf des Servers
+    Serial.println("Client available");
+    String currentLine = "";                // String definieren für die Anfrage des Clients
+
+    while (client.connected()) { // Loop, solange Client verbunden ist
+
+      if (client.available()) {
+        char c = client.read();             // Ein (1) Zeichen der Anfrage des Clients lesen
+        Serial.write(c);                    // und es im Seriellen Monitor ausgeben
+        header += c;
+        if (c == '\n') {                    // bis eine Neue Zeile ausgegeben wird
+
+          // Wenn der Client eine Leerzeile sendet, ist das Ende des HTTP Request erreicht
+          if (currentLine.length() == 0) {
+
+            // Der Server sendet nun eine Antwort an den Client
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+            // Die Webseite anzeigen
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\"></head>");
+            client.println("<body><h1 align=\"center\">Hier spricht dein Server! :)</h1></body></html>");
+
+            // Die Antwort mit einer Leerzeile beenden
+            client.println();
+            // Den Loop beenden
+            break;
+          } else { // Bei einer Neuen Zeile, die Variable leeren
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // alles andere als eine Leerzeile wird
+          currentLine += c;      // der Variable hinzugefüht
+        }
+      }
+    }
+    // Variable für den Header leeren
+    header = "";
+    // Die Verbindung beenden
+    client.stop();
+    Serial.println("Client disconnected");
+    Serial.println("");
+  }
+}
+
 void loop() {
+    makeWebServer();
     if(marker == 1)
       lix.write_fade(zeit+30000);
     else
